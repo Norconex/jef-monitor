16b13f2464e14a4caa7fe2582da8f5c277a79895
Pascal Essiembre <pascal.essiembre@norconex.com>
2014-12-05 23:09:21 -0500

    Prepare for release.

--- norconex-jef-monitor/src/main/scripts/jef-monitor.bat
+++ norconex-jef-monitor/src/main/scripts/jef-monitor.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
+set ROOT_DIR=%~dp0
 
-java -Dfile.encoding=UTF8 -cp "./lib/*;./classes" com.norconex.jefmon.server.JEFMonServer %*
-
+java -Dlog4j.configuration="file:///%ROOT_DIR%classes/log4j.properties" -Dfile.encoding=UTF8 -cp "./lib/*;./classes" com.norconex.jefmon.server.JEFMonServer %*
