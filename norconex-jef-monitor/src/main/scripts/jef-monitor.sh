bf676f10a78a1e7a894a36c1e14f87a9deaccb27
Pascal Essiembre <pascal.essiembre@norconex.com>
2016-07-26 14:26:53 -0400

    Fixed launch shell script when run from script current directory.

--- norconex-jef-monitor/src/main/scripts/jef-monitor.sh
+++ norconex-jef-monitor/src/main/scripts/jef-monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd $(dirname $0)
-export ROOT_DIR=$(dirname $0)
+export ROOT_DIR=$(realpath $(dirname $0))
 
 java -Dlog4j.configuration="file:///${ROOT_DIR}/classes/log4j.properties" -Dfile.encoding=UTF8 -cp "./lib/*:./classes" com.norconex.jefmon.server.JEFMonServer "$@"
